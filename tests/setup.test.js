6c3c942c3c229a40b95da5b857a9461d1cee9e7d
BroTarek <BroTarek@gmail.com>
2026-07-16 22:36:04 +0300

    set up functions

---
+++ tests/setup.test.js
@@ -0,0 +1,22 @@
+describe("set up function",()=>{
+     
+    beforeAll(()=>{
+        //connectToDb()
+    })
+
+    afterAll(()=>{
+        //disconnect()
+    })
+
+    beforeEach(()=>{
+       // exclusive for this test file only
+    })
+
+    afterEach(()=>{
+      // exclusive for this test file only
+    })
+    // instead of repaeting function , use setup functions ,beforeAll(),afterAll()
+    // test("test one",()=>{connectToDb()})
+    // test("test two",()=>{connectToDb()})
+    // test("test three",()=>{connectToDb()})
+})
