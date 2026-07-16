17e21f15e8269948b52c82ad30e5ca792ccdf1f2
BroTarek <BroTarek@gmail.com>
2026-07-16 12:33:46 +0300

    Grouped realted tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,14 +1,17 @@
 const {checknm}= require("../src/main")
 
-test("checknm -should return number passed if input is positive",()=>{
+describe("checknm",()=>{
+
+test("should return number passed if input is positive",()=>{
   const res=checknm(2);
   expect(res).toBe(2);
 })
-test("checknm -should return the number in negative",()=>{
+test("should return the number in negative",()=>{
   const res=checknm(-2);
   expect(res).toBe(-2);
 })
-test("checknm -should return zero",()=>{
+test("should return zero",()=>{
   const res=checknm(0);
   expect(res).toBe(0);
+})
 })
